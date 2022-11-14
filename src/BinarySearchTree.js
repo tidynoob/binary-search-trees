679fe15514546be80c5fb103720e4e951ac92e78
tidynoob <mitchellgriffin9@gmail.com>
2022-11-14 13:18:12 -0500

    Add insert function

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,32 +1,46 @@
 import Node from "./Node";
 
-const BinarySearchTree = (arr) => {
-  // console.log("arr", arr);
+const Tree = (arr) => {
   const sortedArray = arr.sort((a, b) => a - b);
-  // console.log("sortedArray", sortedArray);
   const dedupedArray = sortedArray.filter(
     (item, index) => sortedArray.indexOf(item) === index
   );
-  // console.log("dedupedArray", dedupedArray);
 
   const buildTree = (array) => {
     if (array.length === 0) return null;
 
     const mid = Math.floor(array.length / 2);
     const node = new Node(array[mid]);
-    // console.log("array", array);
-    // console.log("node", node);
-    // console.log("leftArray", array.slice(0, mid));
 
     node.left = buildTree(array.slice(0, mid));
-    // console.log("leftNode", node.left);
+
     node.right = buildTree(array.slice(mid + 1));
-    // console.log("node", node);
 
     return node;
   };
 
-  const prettyPrint = (node, prefix = "", isLeft = true) => {
+  const root = buildTree(dedupedArray);
+
+  const insert = (value, checkNode = root) => {
+    let node = checkNode;
+    if (node === null) {
+      const newNode = new Node(value);
+      node = newNode;
+      // console.log("inserted", value);
+      // console.log("node", node);
+      return node;
+    }
+    if (value < node.value) {
+      node.left = insert(value, node.left);
+    } else if (value > node.value) {
+      node.right = insert(value, node.right);
+    }
+    // if (node.value === value) return node;
+
+    return node;
+  };
+
+  const prettyPrint = (node = root, prefix = "", isLeft = true) => {
     if (node.right !== null) {
       prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
     }
@@ -38,9 +52,10 @@ const BinarySearchTree = (arr) => {
   };
 
   return {
-    root: buildTree(dedupedArray),
-    prettyPrint: prettyPrint(buildTree(dedupedArray)),
+    root,
+    insert,
+    prettyPrint,
   };
 };
 
-export default BinarySearchTree;
+export default Tree;
